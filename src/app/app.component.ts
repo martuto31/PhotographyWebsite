6ecd85016cd8c873e642f842d2157d66ac2395cc
Martin Petrov <martuto31@gmail.com>
2022-07-09 19:40:08 +0300

    Slidoshow logic from app-component to slideshow-component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,42 +9,5 @@ export class AppComponent implements OnInit{
   title = 'VikiPhotography';
 
   ngOnInit(): void {
-    this.slideShow(0);
   }
-
-  slideShow(n: number){
-    var slides = document.getElementsByClassName("slides") as HTMLCollectionOf<HTMLElement>;
-    var slideIndex = n;
-
-    for(var i = 0; i < slides.length; i++)
-    {
-      slides[i].style.display = "none";
-    }
-
-    slideIndex++;
-    //only works with 3 pictures, FIX NEEDED!! - Bad practice
-    if(slideIndex > slides.length)
-    {
-      slideIndex = 1;
-    }
-    console.log(slideIndex - 1);
-    console.log(slideIndex);
-    slides[slideIndex - 1].style.display = "flex";
-    setTimeout(() => {this.slideShow(slideIndex)}, 5000);
-  }
-
-  makeNavbarActive(id: number){ 
-    var navs = document.getElementsByClassName("pricing-nav-bootstrapModify") as HTMLCollectionOf<HTMLElement>;
-  
-    for(var i = 0; i < navs.length; i++)
-    {
-      navs[i].classList.remove("pricing-nav-active");
-    }
-    navs[id].classList.add("pricing-nav-active");
-  }
-
-  // currentSlide(n: number) {
-  //   this.slideShow(n);
-  // }
-
 }
