000871c2b86160310487c403de137d8e47074492
Martin Petrov <martuto31@gmail.com>
2022-08-03 16:19:51 +0300

    Added mobile navbar.

--- src/app/slideshow/slideshow.component.ts
+++ src/app/slideshow/slideshow.component.ts
@@ -1,4 +1,6 @@
+import { variable } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
+import { __classPrivateFieldSet } from 'tslib';
 
 @Component({
   selector: 'app-slideshow',
@@ -14,21 +16,26 @@ export class SlideshowComponent implements OnInit {
   }
 
   slideShow(n: number){
-    var slides = document.getElementsByClassName("slides") as HTMLCollectionOf<HTMLElement>;
+    var slides = document.getElementById("slides");
     var slideIndex = n;
+    var slidesNumber = 3;
 
-    for(var i = 0; i < slides.length; i++)
-    {
-      slides[i].style.display = "none";
-    }
+    // for(var i = 0; i < slides.length; i++)
+    // {
+    //   slides[i].style.display = "none";
+    // }
 
     slideIndex++;
     //only works with 3 pictures, FIX NEEDED!! - Bad practice
-    if(slideIndex > slides.length)
+    if(slideIndex > slidesNumber)
     {
       slideIndex = 1;
+      slides?.classList.add("bg-1");
+      slides?.classList.remove("bg-3");
     }
-    slides[slideIndex - 1].style.display = "flex";
+    slides?.classList.add("bg-" + slideIndex);
+    slides?.classList.remove("bg-" + (slideIndex-1));
+    // slides[slideIndex - 1].style.display = "flex";
     setTimeout(() => {this.slideShow(slideIndex)}, 3500);
   }
 
@@ -45,4 +52,24 @@ export class SlideshowComponent implements OnInit {
   // currentSlide(n: number) {
   //   this.slideShow(n);
   // }
+
+  //Hamburger menu
+  onToggle(){
+    const primaryNav = document.getElementById('home-nav');
+    const navToggle = document.querySelector('.mobile-nav-toggle');
+
+    const visibility = primaryNav?.getAttribute("data-visible");
+
+    if(visibility === "false")
+    {
+      primaryNav?.setAttribute("data-visible", "true");
+      navToggle?.setAttribute("aria-expanded", "true");
+    }
+    else if(visibility === "true")
+    {
+      primaryNav?.setAttribute("data-visible", "false");
+      navToggle?.setAttribute("aria-expanded", "fa;se");
+    }
+    console.log(visibility);
+  }
 }
