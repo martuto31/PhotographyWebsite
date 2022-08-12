9b7861f09768c05cd288b6a9290ca86bf698a840
Martin Petrov <martuto31@gmail.com>
2022-08-12 19:28:27 +0300

    Portfolio nav mobile and tablet size toggle menu added.

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -12,4 +12,23 @@ export class PortfolioComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onToggle(){
+    const primaryNav = document.getElementById('portfolio-container');
+    const navToggle = document.querySelector('.portfolio-nav ul');
+
+    const visibility = primaryNav?.getAttribute("data-visible");
+
+    if(visibility === "false")
+    {
+      primaryNav?.setAttribute("data-visible", "true");
+      navToggle?.setAttribute("aria-expanded", "true");
+    }
+    else if(visibility === "true")
+    {
+      primaryNav?.setAttribute("data-visible", "false");
+      navToggle?.setAttribute("aria-expanded", "fa;se");
+    }
+    console.log(visibility);
+  }
+
 }
