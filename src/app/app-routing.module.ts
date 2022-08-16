b9bd6eac8664feb9b5a6d697eca1e3cc1d619fd2
Martin Petrov <martuto31@gmail.com>
2022-08-16 03:26:32 +0300

    Krisi gallery added and all portfolio links added to app-route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,10 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { GraduationComponent } from './portfolio/graduation/graduation.component';
 import { MeriGraduationComponent } from './portfolio/graduation/meri-graduation/meri-graduation.component';
+import { VanesaGalleryComponent } from './portfolio/graduation/vanesa-gallery/vanesa-gallery.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
+import { KatiGalleryComponent } from './portfolio/portrait/kati-gallery/kati-gallery.component';
+import { KrisiGalleryComponent } from './portfolio/portrait/krisi-gallery/krisi-gallery.component';
 import { PortraitComponent } from './portfolio/portrait/portrait.component';
 import { VanesaStanislavGalleryComponent } from './portfolio/wedding/vanesa-stanislav-gallery/vanesa-stanislav-gallery.component';
 import { WeddingComponent } from './portfolio/wedding/wedding.component';
@@ -15,8 +18,11 @@ const routes: Routes = [
     {path: 'weddings', component: WeddingComponent},
     {path: 'weddings-vanesa-stanislav-gallery', component: VanesaStanislavGalleryComponent},
     {path: 'graduation', component: GraduationComponent},
-    {path: 'graduation-meri', component: MeriGraduationComponent},
+    {path: 'graduation-meri-gallery', component: MeriGraduationComponent},
+    {path: 'graduation-vanesa-gallery', component: VanesaGalleryComponent},
     {path: 'portrait', component: PortraitComponent},
+    {path: 'portrait-krisi-gallery', component: KrisiGalleryComponent},
+    {path: 'portrait-kati-gallery', component: KatiGalleryComponent},
   ]},
 ];
 
