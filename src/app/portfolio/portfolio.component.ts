fdc8ce2706f81735f7f8e3bb648dee636f782ad9
Martin Petrov <martuto31@gmail.com>
2022-09-01 02:59:31 +0300

    Portfolio links nav now compatible with mobile.

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -12,21 +12,37 @@ export class PortfolioComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  // primary navbar and links nav menu toggle for mobile
   onToggle(){
     const primaryNav = document.getElementById('portfolio-container');
-    const navToggle = document.querySelector('.portfolio-nav ul');
+    const linksNav = document.getElementById('links-container');
+    const primaryNavToggle = document.querySelector('.portfolio-nav ul');
+    const linksToggle = document.querySelector('.links-nav ul');
 
-    const visibility = primaryNav?.getAttribute("data-visible");
+    const primaryNavVisibility = primaryNav?.getAttribute("data-visible");
+    const linksNavVisibility = linksNav?.getAttribute("data-visible");
 
-    if(visibility === "false")
+    if(primaryNavVisibility === "false")
     {
       primaryNav?.setAttribute("data-visible", "true");
-      navToggle?.setAttribute("aria-expanded", "true");
+      primaryNavToggle?.setAttribute("aria-expanded", "true");
     }
-    else if(visibility === "true")
+    else if(primaryNavVisibility === "true")
     {
       primaryNav?.setAttribute("data-visible", "false");
-      navToggle?.setAttribute("aria-expanded", "false");
+      primaryNavToggle?.setAttribute("aria-expanded", "false");
+    }
+
+
+    if(linksNavVisibility === "false")
+    {
+      linksNav?.setAttribute("data-visible", "true");
+      linksToggle?.setAttribute("aria-expanded", "true");
+    }
+    else if(linksNavVisibility === "true")
+    {
+      linksNav?.setAttribute("data-visible", "false");
+      linksToggle?.setAttribute("aria-expanded", "false");
     }
   }
 
