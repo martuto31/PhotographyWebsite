0ec494dbbf126f8b0edc4fbe5aa1ddb729a65f3e
Martin Petrov <martuto31@gmail.com>
2022-09-03 21:19:30 +0300

    Popup image functionallity added in vanesa and stanislav gallery.

--- src/app/portfolio/wedding/vanesa-stanislav-gallery/vanesa-stanislav-gallery.component.ts
+++ src/app/portfolio/wedding/vanesa-stanislav-gallery/vanesa-stanislav-gallery.component.ts
@@ -10,6 +10,37 @@ export class VanesaStanislavGalleryComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.imagePopup();
   }
 
+  imagePopup(){
+    var imgs = document.getElementsByClassName('img');
+    for(var i = 0; i < imgs.length; i++)
+    {
+      if(imgs[i] != undefined)
+      {
+        imgs[i].addEventListener("click", function(el){
+          popUp(el);
+        }, false)
+      }
+    }
+
+    function popUp(el: any){
+      (document.querySelector('.popup-image') as HTMLInputElement).style.display = 'block';
+      var imageSrc = (document.getElementById(el.target.id) as HTMLImageElement).src;
+      if(imageSrc != undefined)
+      {
+        document.getElementById('popup-image')?.setAttribute('src', imageSrc);
+      }
+    }
+  }
+
+  closePopup(){
+    var popUpImage = document.getElementById('popup-image-div');
+    
+    if(popUpImage != undefined)
+    {
+      popUpImage.style.display = 'none';
+    }
+  }
 }
