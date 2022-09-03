94eca9924dfaa4aea32e287825fa4e5fe9e5a89e
Martin Petrov <martuto31@gmail.com>
2022-09-03 22:51:48 +0300

    Popup image functionallity added in all galleries.

--- src/app/portfolio/graduation/meri-graduation/meri-graduation.component.ts
+++ src/app/portfolio/graduation/meri-graduation/meri-graduation.component.ts
@@ -10,6 +10,38 @@ export class MeriGraduationComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.imagePopup();
+  }
+
+  imagePopup(){
+    var imgs = document.getElementsByClassName('img');
+    for(var i = 0; i < imgs.length; i++)
+    {
+      if(imgs[i] != undefined)
+      {
+        imgs[i].addEventListener("click", function(el){
+          popUp(el);
+        }, false)
+      }
+    }
+
+    function popUp(el: any){
+      (document.querySelector('.popup-image') as HTMLInputElement).style.display = 'flex';
+      var imageSrc = (document.getElementById(el.target.id) as HTMLImageElement).src;
+      if(imageSrc != undefined)
+      {
+        document.getElementById('popup-image')?.setAttribute('src', imageSrc);
+      }
+    }
+  }
+
+  closePopup(){
+    var popUpImage = document.getElementById('popup-image-div');
+    
+    if(popUpImage != undefined)
+    {
+      popUpImage.style.display = 'none';
+    }
   }
 
 }
